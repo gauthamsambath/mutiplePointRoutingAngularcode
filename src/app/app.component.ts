63a0eee9af46f2d5e6213b1cdda80d784cfc5dbf
gautham.sambath <gautham.sambath@cgi.com>
2019-11-26 11:04:37 +0530

    indentations corrected,cleaned code and hardcoded center value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { RoutingapiserviceService } from './routingapiservice.service';
 })
 export class AppComponent implements OnInit {
   title = 'Route-Optimizer-Interface';
-  addresses = ["koramangala","marathahalli","forum%20mall","indiranagar%20banglore"]
+  addresses = ["koramangala","marathahalli","forum%20mall","indiranagar%20banglore","mahadeveapura%20banglore"]
   jsonresponse;
   allcoordinates;
   points=[]
@@ -22,152 +22,136 @@ export class AppComponent implements OnInit {
   checkforbranching="this is to see if branching and updation have happened or not"
   constructor(private routeapiservice: RoutingapiserviceService, private zone: NgZone) { }
 
-  ngOnInit() {
-    // map.accessToken = 'pk.eyJ1IjoiZ2F1dGhhbTk5IiwiYSI6ImNrMzRlMmxrNjE0ZTMzbXBhOWRwdDk1eTcifQ.-ZceQ8jARpf90y0tJnQhoQ';
-    // (map as any).accessToken = 'pk.eyJ1IjoiZ2F1dGhhbTk5IiwiYSI6ImNrMzRlMmxrNjE0ZTMzbXBhOWRwdDk1eTcifQ.-ZceQ8jARpf90y0tJnQhoQ'; 
-
-
-      this.routeapiservice.getLatandLong().subscribe((data) => {
-              this.zone.run(()=>{
-                this.allcoordinates=data
-                this.coordinates=this.updatevalue(data)
-                updatepointsonmap(this.coordinates,this.addresses)
-                console.log(this.coordinates)
-                this.routeapiservice.getGeoJsonLatLOng(this.coordinates).subscribe((data2)=>{
-                    this.zone.run(()=>{
+  ngOnInit() 
+    {
+      this.routeapiservice.getLatandLong(this.addresses).subscribe((data) => 
+        {
+          this.zone.run(()=>
+            {
+              this.allcoordinates=data
+              this.coordinates=this.updatevalue(data)
+              updatepointsonmap(this.coordinates,this.addresses)
+              setMapCenter(mapp,this.coordinates)
+              console.log(this.coordinates)
+              this.routeapiservice.getGeoJsonLatLOng(this.coordinates,this.addresses).subscribe((data2)=>
+                {
+                  this.zone.run(()=>
+                    {
                       this.waypoints=data2
                       makegeojsonline(data2,this.addresses.length-1)
-                      // temp2.resourceSets[0].resources[0].routePath.line.coordinates
-                      // console.log(data2)
                     })
-                  })
-
-              
-                
-                // console.log(data)
-              })
-
-      })
-  
-
-        //     for(i=0;i<this.addresses.length;i++)
-        //     {
-        //   var lat = this.allcoordinates[i].resourceSets.resources[0].point.coordinates[1]
-        //   var long = this.allcoordinates[i].resourceSets.resources[0].point.coordinates[0]
-        //             // console.log(this.jsonresponse[0].resources[0].point.coordinates
-        //   var geomarker = new mapboxgl.Marker({
-        //     draggable: true,
-        //   })
-          
-        //     .setLngLat([lat, long])
-        //     .addTo(mapp);
-        // }
- 
-    // console.log(this.allcoordinates)
-    var coordinates = document.getElementById('coordinates');
-    Object.getOwnPropertyDescriptor(map, "accessToken").set('pk.eyJ1IjoiZ2F1dGhhbTk5IiwiYSI6ImNrMzRlMmxrNjE0ZTMzbXBhOWRwdDk1eTcifQ.-ZceQ8jARpf90y0tJnQhoQ');
-    let mapp = new map.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.50, 40],
-      zoom: 9 // starting zoom
-
-    }
-    );
-
-
-
-    var geomarker = new mapboxgl.Marker({
-      draggable: true,
-    }).setLngLat([-74.50, 40])
-      .addTo(mapp);
-
-    var geocoder = new MapboxGeocoder({
-      accessToken: map.accessToken,
-      localGeocoder: forwardGeocoder,
-      zoom: 14,
-      placeholder: "Enter search e.g. Lincoln Park",
-      mapboxgl: mapboxgl,
-      marker: false,
-    });
-    console.log(geocoder)
-    mapp.addControl(geocoder);
-    function onDragEnd() {
-      var lngLat = geomarker.getLngLat();
-      coordinates.style.display = 'block';
-      coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-      // console.log('Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat)
-    }
-    // function result()
-    //   {
-    //     var lngLat = MapboxGeocoder.getLngLat();
-    //     coordinates.style.display = 'block';
-    //     coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-    //     console.log('Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat)
-    //   }  
-    geomarker.on('dragend', onDragEnd);
-    geocoder.on('results', function (result) {
-      console.log(result.features[0].center)
-      coordinates.style.display = 'block';
-      var longitude = result.features[0].center[0];
-      var latitude = result.features[0].center[1]
-      coordinates.innerHTML = 'Longitude: ' + longitude + '<br />Latitude: ' + latitude;
-      geomarker.setLngLat([longitude, latitude]);
-    });
-    function makegeojsonline(coordinateset,lengthofset)
-      {
-        mapp.on('load', function (){
-          for(var i=0;i<lengthofset;i++)
-          {
-            // console.log(coordinateset[i].resourceSets[0].resources[0].routePath.line.coordinates)
-            var actualcordinates=coordinateset[i].resourceSets[0].resources[0].routePath.line.coordinates
-            var revcoordinates=[]
-            // console.log(actualcordinates)
-            for(var j=0;j<actualcordinates.length;j++)
-              {
-                revcoordinates[j]=[actualcordinates[j][1],actualcordinates[j][0]]
-              }
-            console.log(revcoordinates)  
-            mapp.addLayer({
-              "id": "route"+i,
-              "type": "line",
-              "source": {
-              "type": "geojson",
-              "data": {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-              "type": "LineString",
-              "coordinates":revcoordinates
-              }
-              }
-              },
-              "layout": {
-              "line-join": "round",
-              "line-cap": "round"
-              },
-              "paint": {
-              "line-color": "#ff0808",
-              "line-width": 5
-              }
-              });
-          }
-
+                })
+            })
         })
-      
+      var coordinates = document.getElementById('coordinates');
+      Object.getOwnPropertyDescriptor(map, "accessToken").set('pk.eyJ1IjoiZ2F1dGhhbTk5IiwiYSI6ImNrMzRlMmxrNjE0ZTMzbXBhOWRwdDk1eTcifQ.-ZceQ8jARpf90y0tJnQhoQ');
+      let mapp = new map.Map(
+        {
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [77.65542724609375, 12.940049667358398],
+          zoom: 11
+        });
 
-          }
-         function updatepointsonmap(coordinates,addresses)
-          {
-            for(var i=0;i<addresses.length;i++)
+
+      var geomarker = new mapboxgl.Marker(
+        {
+          draggable: true,
+          color:"orange"
+        })
+        .setLngLat([77.65542724609375, 12.940049667358398])
+        .addTo(mapp);
+
+      var geocoder = new MapboxGeocoder(
+        {
+          accessToken: map.accessToken,
+          localGeocoder: forwardGeocoder,
+          zoom: 14,
+          placeholder: "Enter search e.g. Lincoln Park",
+          mapboxgl: mapboxgl,
+          marker: false,
+        });
+      console.log(geocoder)
+      mapp.addControl(geocoder);
+      function onDragEnd() 
+        {
+          var lngLat = geomarker.getLngLat();
+          coordinates.style.display = 'block';
+          coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+        }
+      geomarker.on('dragend', onDragEnd);
+      geocoder.on('results', function (result) 
+        {
+          console.log(result.features[0].center)
+          coordinates.style.display = 'block';
+          var longitude = result.features[0].center[0];
+          var latitude = result.features[0].center[1]
+          coordinates.innerHTML = 'Longitude: ' + longitude + '<br />Latitude: ' + latitude;
+          geomarker.setLngLat([longitude, latitude]);
+        });
+      function makegeojsonline(coordinateset,lengthofset)
+        {
+          mapp.on('load', function ()
             {
-              var geomarker = new mapboxgl.Marker({
-                draggable: true,
-              }).setLngLat([coordinates[i][1],coordinates[i][0]])
+              for(var i=0;i<lengthofset;i++)
+                {
+                  var actualcordinates=coordinateset[i].resourceSets[0].resources[0].routePath.line.coordinates
+                  var revcoordinates=[]
+                  for(var j=0;j<actualcordinates.length;j++)
+                    {
+                      revcoordinates[j]=[actualcordinates[j][1],actualcordinates[j][0]]
+                    }
+                  console.log(revcoordinates)  
+                  mapp.addLayer(
+                    {
+                      "id": "route"+i,
+                      "type": "line",
+                      "source": 
+                        {
+                          "type": "geojson",
+                          "data": 
+                            {
+                              "type": "Feature",
+                              "properties": {},
+                              "geometry": 
+                                {
+                                  "type": "LineString",
+                                  "coordinates":revcoordinates
+                                }
+                            }
+                          },
+                      "layout": 
+                        {
+                          "line-join": "round",
+                          "line-cap": "round"
+                        },
+                      "paint": 
+                        {
+                          "line-color": "#ff0808",
+                        "line-width": 5
+                        } 
+                    });
+                }
+            })
+        }
+      function updatepointsonmap(coordinates,addresses)
+        {
+          for(var i=0;i<addresses.length;i++)
+            {
+              var geomarker = new mapboxgl.Marker(
+                {
+                  draggable: true,
+                })
+                .setLngLat([coordinates[i][1],coordinates[i][0]])
                 .addTo(mapp);
             } 
-          }     
-      }
+        }  
+      function setMapCenter(map,coordinates)
+        {
+          console.log(coordinates[0])
+          map.center=[coordinates[0][1],coordinates[0][0]]
+        }
+    }
   
   updatevalue(data)
     {
